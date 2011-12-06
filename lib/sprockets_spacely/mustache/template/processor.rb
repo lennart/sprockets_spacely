25e38d0134e4b0d8a2c610e1337f05247108e7f3
Lennart Melzer <me@lmaa.name>
2011-12-06 16:52:07 +0100

    move all code into just the processor

--- lib/sprockets_spacely/mustache/template/processor.rb
+++ lib/sprockets_spacely/mustache/template/processor.rb
@@ -8,14 +8,34 @@ module Sprockets
           'application/javascript'
         end
 
+        def self.default_namespace
+          "this.MustacheTemplates"
+        end
+
+        def self.default_library
+          "jQuery"
+        end
+
+        attr_reader :namespace, :library
+
         def prepare
+          @namespace = self.class.default_namespace
+          @library = self.class.default_library
         end
 
         def evaluate(scope, locals, &block)
-          namespace = Namespace.value || "window"
-          library = Library.value || "jQuery"
-
           Generator.new(namespace, scope.logical_path, escape_javascript(data), library).generate
+
+
+          js_function = <<-JS
+(function($) {
+  #{namespace} || (#{namespace} = {});
+
+  #{namespace}[#{scope.logical_path.inspect}] = function(obj, partials) {
+    return Mustache.to_html("#{escape_javascript template_string}", obj, partials);
+  };
+}(#{library}));
+          JS
         end
       end
     end
